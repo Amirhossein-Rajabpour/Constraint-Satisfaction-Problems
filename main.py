32632734ede41fb4f14359d2c8cfe4add533b831
Armin Zolfaghari Daryani <arminzd1@gmail.com>
2021-07-19 22:09:05 +0430

    before merge

--- main.py
+++ main.py
@@ -1,21 +1,11 @@
-import numpy as np
-import CSP
 
-def take_board(file):
 
-    dimension = f.readline()[0]
-    puzzle = f.readlines()
-    board = []
-    for row in puzzle:
-        board.append(row.replace('1\n', '1').replace('0\n', '0').replace('-\n', '-').split(' '))
-    board = np.array(board)
-    return board, dimension
+def start_solving_puzzle():
+    node = 11
+    while node.finish
 
-if __name__ == '__main__':
 
-    file_name = 'puzzles/puzzle0.txt'
-    f = open(file_name, "r")
-    board, dimension = take_board(f)
 
-    const_prop_mode = 'forwardchecking'
-    CSP.start_CSP(board, dimension, const_prop_mode)
+if __name__ == "__main__":
+    # create Object node
+    # start solving puzzle
