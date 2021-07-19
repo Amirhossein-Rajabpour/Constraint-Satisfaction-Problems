8ce45bff5eab88ce3238f2a31f292ee466c200a2
Armin Zolfaghari Daryani <arminzd1@gmail.com>
2021-07-19 22:39:36 +0430

    add Mian.py

---
+++ Main.py
@@ -0,0 +1,20 @@
+import CSP
+
+def take_board(file):
+
+    dimension = f.readline()[0]
+    puzzle = f.readlines()
+    board = []
+    for row in puzzle:
+        board.append(row.replace('1\n', '1').replace('0\n', '0').replace('-\n', '-').split(' '))
+    return board
+
+if "__name__" == 'main':
+
+    file_name = 'puzzles/puzzle0.txt'
+    f = open(file_name, "r")
+    board = take_board(f)
+    print(board)
+
+    const_prop_mode = 'forwardchecking'
+    CSP.start_CSP(board, const_prop_mode)
