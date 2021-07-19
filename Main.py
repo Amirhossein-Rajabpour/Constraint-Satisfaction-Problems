778d59b2c6c4f2ead3abcd70715f066fde39a2f9
Armin Zolfaghari Daryani <arminzd1@gmail.com>
2021-07-19 23:09:31 +0430

    change Main.py

--- Main.py
+++ Main.py
@@ -1,20 +1,19 @@
 import CSP
 
-def take_board(file):
 
-    dimension = f.readline()[0]
-    puzzle = f.readlines()
-    board = []
-    for row in puzzle:
-        board.append(row.replace('1\n', '1').replace('0\n', '0').replace('-\n', '-').split(' '))
-    return board
+def take_board(filename):
+    with open(filename) as file:
+        dimension = file.readline()[0]
+        puzzle = file.readlines()
+        board = []
+        for row in puzzle:
+            board.append(row.replace('1\n', '1').replace('0\n', '0').replace('-\n', '-').split(' '))
+        return board
 
-if "__name__" == 'main':
 
-    file_name = 'puzzles/puzzle0.txt'
-    f = open(file_name, "r")
-    board = take_board(f)
-    print(board)
+if __name__ == "__main__":
+    file_name = './puzzles/puzzle0.txt'
+    board = take_board(file_name)
 
-    const_prop_mode = 'forwardchecking'
+    const_prop_mode = 'forward_checking'
     CSP.start_CSP(board, const_prop_mode)
