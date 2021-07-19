40f8b8c6452dea2cae97601198367b02e34ee967
Amirhossein Rajabpour <rajabpouramirhosein@gmail.com>
2021-07-19 21:25:26 +0430

    refactor all the domains dict to list. switching to parents should be handled.

--- CSP.py
+++ CSP.py
@@ -5,52 +5,56 @@
 import Heuristic
 import Propagation
 import Node
-import math
+import copy
 
 assignment = []
-
-def is_end(node):
-    pass
 
 def find_path(node):
     path = ''
     return path
 
-def create_domains_dict(initial_board, dimension):
-    domains_dict = {}
-    dimension = int(dimension)
-    for i in range(1, (dimension**2)+1):
-        x = math.ceil(i/dimension) - 1
-        y = (i % dimension) - 1
-        if initial_board[x][y] == '1' or initial_board[x][y] == '0':
-            domains_dict[i] = []
-        else:
-            domains_dict[i] = ['0', '1']
-    return domains_dict
+def create_domains_list(initial_board):
+    domains_list = copy.deepcopy(initial_board)
+    dimension, _ = len(domains_list)
+
+    for i in range(dimension):
+        for j in range(dimension):
+            if domains_list[i][j] == '1':
+                domains_list[i][j] = '1'
+            elif domains_list[i][j] == '0':
+                domains_list[i][j] = '0'
+            else:
+                domains_list[i][j] = ['0', '1']
+
+    return domains_list
 
 
 # it takes raw input and create basic structures for the program
-def start_CSP(input_board, dimension, const_prop_mode):
-    # TODO create domains dict
-    initial_node = Node.Node(input_board, '')  # initial node does not have parent
-    domains_dict = create_domains_dict(input_board, dimension)
-    assignment.append(initial_node)
-    CSP_Backtracking(domains_dict, const_prop_mode)
+def start_CSP(input_board, const_prop_mode):
 
-def CSP_Backtracking(domains_dict, const_prop_mode):
+    domains_list = create_domains_list(input_board)
+    initial_node = Node.Node(input_board, '', domains_list, '', '')  # initial node does not have parent
+    # assignment.append(initial_node)
+    CSP_Backtracking(initial_node, const_prop_mode)
+
+def CSP_Backtracking(node, const_prop_mode):
 
     while True:
-        cell = Heuristic.MRV(domains_dict)
-        assignment.append(cell)
-        if const_prop_mode == 'forwardchecking':
-            domains_dict = Propagation.forwardchecking(cell, assignment)    # cell is the last element of assignment array i guess
-        elif const_prop_mode == 'MAC':
-            domains_dict = Propagation.MAC(cell, assignment)    # cell is the last element of assignment array i guess
-
-        if not is_end(assignment[-1]):
-            CSP_Backtracking(domains_dict, const_prop_mode)
+        not_empty, node = Heuristic.MRV(node)
+        if not not_empty:
+            # here we should go to parent node
+            pass
         else:
-            # find path takes answer node as input
-            path = find_path(assignment[-1])
-            # not sure to return path or final node!
-            return path
+            if const_prop_mode == 'forwardchecking':
+                state, domains_list = Propagation.forward_checking(node)
+            elif const_prop_mode == 'MAC':
+                state, domains_list = Propagation.MAC(node)
+
+
+
+        # if not is_end(assignment[-1]):
+        #     CSP_Backtracking(domains_list, const_prop_mode)
+        # else:
+        #     # find path takes answer node as input
+        #     path = find_path(assignment[-1])
+        #     return path
