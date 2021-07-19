3bb587625679de3ab3f62c53018c9b6aac346d57
Amirhossein Rajabpour <rajabpouramirhosein@gmail.com>
2021-07-19 13:30:36 +0430

    handle input

--- main.py
+++ main.py
@@ -1,16 +1,14 @@
-# This is a sample Python script.
+import numpy as np
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    file_name = 'puzzles/puzzle0.txt'
+    f = open(file_name, "r")
+    dimension = f.readline()[0]
+    puzzle = f.readlines()
+    # for row in puzzle: print(row[0])
+    board = []
+    for row in puzzle:
+        n_row = row.replace('1\n', '1').replace('0\n', '0').replace('-\n', '-').split(' ')
+        board.append(n_row)
+    board = np.array(board)
