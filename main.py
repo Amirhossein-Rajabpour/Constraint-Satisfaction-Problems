d9534c165aabe6fd1e447feb2c3a6eda1e38fa0b
Armin Zolfaghari Daryani <arminzd1@gmail.com>
2021-07-19 22:26:04 +0430

    Main

--- main.py
+++ main.py
@@ -1,20 +1,11 @@
-import CSP
 
-def take_board(file):
 
-    dimension = f.readline()[0]
-    puzzle = f.readlines()
-    board = []
-    for row in puzzle:
-        board.append(row.replace('1\n', '1').replace('0\n', '0').replace('-\n', '-').split(' '))
-    return board
+def start_solving_puzzle():
+    node = 11
+    while node.finish
 
-if name == 'main':
 
-    file_name = 'puzzles/puzzle0.txt'
-    f = open(file_name, "r")
-    board = take_board(f)
-    print(board)
 
-    const_prop_mode = 'forwardchecking'
-    CSP.start_CSP(board, const_prop_mode)
+if __name__ == "__main__":
+    # create Object node
+    # start solving puzzle
