714e918da06e1869e63d7f18d78465d3e7dc2915
Amirhossein Rajabpour <rajabpouramirhosein@gmail.com>
2021-07-21 10:31:32 +0430

    timer changed and report completed

--- Main.py
+++ Main.py
@@ -13,13 +13,14 @@ def take_board(filename):
 
 
 if __name__ == "__main__":
-    file_name = './puzzles/puzzle5.txt'
+    file_name = './puzzles/puzzle6.txt'
     board = take_board(file_name)
 
     starting_time = time.time()
-    const_prop_mode = "MAC"
+    const_prop_mode = "forward-checking"
+    # const_prop_mode = "MAC"
     CSP.start_CSP(board, const_prop_mode)
     ending_time = time.time()
     duration = ending_time - starting_time
     print("\n\nTime analysis")
-    print(f'for constraint propagation {const_prop_mode} took {duration} secs long')
+    print(f'for constraint propagation {const_prop_mode} took ' + "{:.4f}".format(duration) + 'secs long')
